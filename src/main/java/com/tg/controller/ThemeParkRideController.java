b88c36bcaf6d06a6d2144bfdc3eb66f914f015c5
vignesh-somasundaram <vignesh_s@trimble.com>
2021-03-31 15:51:27 +0530

    Third commit

--- src/main/java/com/tg/controller/ThemeParkRideController.java
+++ src/main/java/com/tg/controller/ThemeParkRideController.java
@@ -10,7 +10,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 
 @RestController
-public class ThemeParkRideController {
+public class ThemeParkRideController  {
     private final ThemeParkRideRepository themeParkRideRepository;
 
     public ThemeParkRideController(ThemeParkRideRepository themeParkRideRepository) {
